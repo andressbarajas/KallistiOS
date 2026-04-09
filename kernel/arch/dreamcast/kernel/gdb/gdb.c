b39bc4507bf1ea0ad8f4c0e8a38c3cf9cd014a62
Andress Barajas <andress.barajas@gmail.com>
2026-04-09 14:48:44 -0700

    gdb: add thread query packets

--- kernel/arch/dreamcast/kernel/gdb/gdb.c
+++ kernel/arch/dreamcast/kernel/gdb/gdb.c
@@ -217,6 +217,8 @@ static void gdb_handle_exception(int exception_vector) {
                 break;
             case 'p': handle_read_reg(ptr); break;
             case 'P': handle_write_reg(ptr); break;
+            case 'q': handle_query(ptr); break;
+            case 'T': handle_thread_alive(ptr); break;
             case 'g': handle_read_regs(ptr); break;
             case 'G': handle_write_regs(ptr); break;
             case 'm': handle_read_mem(ptr); break;
